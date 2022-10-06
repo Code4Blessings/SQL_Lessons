f905f49cf2c0076364c14fa1a512fb0aa08d5447
Code4Blessings <robincw35@gmail.com>
2022-10-06 05:45:10 -0400

    Created fruit imports data file

--- GroupBy.sql
+++ GroupBy.sql
@@ -73,7 +73,7 @@ GROUP BY department
 HAVING count(*) < 35
 ORDER BY department
 
---Assignments
+--Exercises
 
 --How many people have the same first names in the company that is greater than 1?
 
@@ -102,3 +102,40 @@ SELECT gender, region_id, MIN(salary) as min_salary, MAX(salary) as max_salary, 
 FROM employees
 GROUP BY gender, region_id
 ORDER BY gender, region_id
+
+--Assignment #5
+
+--Write a query that displays only the state with the largest amount of fruit supply.
+
+
+
+--Write a query that returns the most expensive cost_per_unit of every season. The query should display 2 columns, the season and the cost_per_unit
+
+
+
+--Write a query that returns the state that has more than 1 import of the same fruit.
+
+
+
+--Write a query that returns the seasons that produce either 3 fruits or 4 fruits.
+
+
+
+--Write a query that takes into consideration the supply and cost_per_unit columns for determining the total cost and returns the most expensive state with the total cost.
+
+
+
+--Execute the below SQL script and answer the question that follows:
+
+CREATE table fruits (fruit_name varchar(10));
+INSERT INTO fruits VALUES ('Orange');
+INSERT INTO fruits VALUES ('Apple');
+INSERT INTO fruits VALUES (NULL);
+INSERT INTO fruits VALUES (NULL);
+
+
+
+--Write a query that returns the count of 4. You'll need to count on the column fruit_name and not use COUNT(*)
+
+--HINT: You'll need to use an additional function inside of count to make this work.
+
